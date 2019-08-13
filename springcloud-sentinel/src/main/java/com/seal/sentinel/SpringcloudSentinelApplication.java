0bdfa0be860ee604ea95c4458b34967114b985f3
youngMen1 <fengzhiqiangzxx199644>
2019-08-13 17:20:17 +0800

    desc:sentinel

--- springcloud-sentinel/src/main/java/com/seal/sentinel/SpringcloudSentinelApplication.java
+++ springcloud-sentinel/src/main/java/com/seal/sentinel/SpringcloudSentinelApplication.java
@@ -2,12 +2,15 @@ package com.seal.sentinel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @author zhiqiang.feng
  * @version 1.0
  * @date-time 2019/02/13-11:01
  * @description Sentinel 流量控制、熔断降级
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringcloudSentinelApplication {
 
