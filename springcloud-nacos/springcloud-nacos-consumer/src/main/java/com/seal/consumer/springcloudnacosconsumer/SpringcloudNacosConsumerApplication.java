79a8529ad7c247b52a9b9fde48d3c762bd4b9be4
youngMen1 <fengzhiqiangzxx199644>
2019-08-13 12:09:33 +0800

    Initial commit

---
+++ springcloud-nacos/springcloud-nacos-consumer/src/main/java/com/seal/consumer/springcloudnacosconsumer/SpringcloudNacosConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.seal.consumer.springcloudnacosconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudNacosConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudNacosConsumerApplication.class, args);
+    }
+
+}
